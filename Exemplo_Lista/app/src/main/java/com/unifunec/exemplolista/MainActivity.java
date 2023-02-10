af7ec62c00ae11f9ef6a13bc9c65da9fb5679b66
Marcos Antonio Estremote <52921427+estremot@users.noreply.github.com>
2023-02-10 16:28:03 -0300

    Lista de Alunos
    Lista de Alunos - Parte 1

---
+++ Exemplo_Lista/app/src/main/java/com/unifunec/exemplolista/MainActivity.java
@@ -0,0 +1,29 @@
+package com.unifunec.exemplolista;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Toast.makeText(this,"UNIFUNEC",Toast.LENGTH_LONG).show();
+        setTitle("Lista de Alunos");
+        List<String> alunos = new ArrayList<>(
+                Arrays.asList("Marcos", "Otoni","Codinhoto")
+        );
+
+        ListView lista_Alunos = findViewById(R.id.lst_Alunos);
+        lista_Alunos.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,alunos));
+    }
+}
