f96a2a1c4d39eb32bf4bbaeb39989e5057d2d496
Marcos Antonio Estremote <52921427+estremot@users.noreply.github.com>
2023-04-14 20:01:11 -0300

    AULA 14_04
    Consumindo API em aplicações Android com Retrofit

---
+++ Aula 16_03/banco.sql
@@ -0,0 +1,12 @@
+﻿create database api_unifunec_2023;
+
+create table aluno(
+	codaluno serial primary key,
+	nomealuno varchar(80) not null,
+	curso varchar(80) not null
+);
+
+insert into aluno(nomealuno, curso) values 
+('MARCOS ANTONIO ESTREMOTE','ADS' ),
+('FÁBIO BORIS','ENFERMAGEM' ),
+('JOSÉ PAULO CODINHOTO','MEDICINA' );
